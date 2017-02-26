296f5fb1d298f6b97cde5c595e490c4e57a8f4aa
Matt Pillar <matt@pillarcomputing.com>
2017-02-25 18:56:43 -0800

    Prod deploy not planned at the moment

--- go/constants.go
+++ go/constants.go
@@ -1,10 +1,4 @@
 package main
 
-// TODO make these env variables.
-// Dev
 var RestEndpoint = "http://localhost:4000/api/v1"
 var WebEndpoint = "http://localhost:8000"
-
-// Prod
-//var RestEndpoint = "https://stravasight.com/api/v1"
-//var WebEndpoint = "https://stravasight.com"
--- js/constants/Constants.js
+++ js/constants/Constants.js
@@ -1,11 +1,7 @@
 var keyMirror = require('keymirror');
 
 module.exports = {
-    // TODO make these env variables.
-    // Dev
     RestEndpoint: 'http://localhost:4000/api/v1',
-    // Prod
-    //RestEndpoint: 'https://stravasight.com/api/v1',
 
     ActionTypes: Object.freeze(keyMirror({
         // Sessions
